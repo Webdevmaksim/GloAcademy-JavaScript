13a4918ec0c91d53526b5f414f1236961ea5e543
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2021-03-03 15:19:22 +0300

    Закончил первое домашнее задание и добавил фаил .jshint для настройки javsScript

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-alert('Hello friends2');
+/* Создание переменных */
+let money = '$';
+
+let income = '$';
+
+let addExpenses = '$';
+
+let deposit = '$';
+
+let misson = '600';
+
+let period = 'month';
+
+/* Вывод модального окна alert */
+alert('Привет мир!');
+
+/* Вывод в консоль */
+console.log('Я вывел сообщение в консоль');
